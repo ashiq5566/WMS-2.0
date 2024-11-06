60ba91ab0f540f31f8dc4af1521e2aab0cc1c24a
ashiq <ashiq5566.k@gmail.com>
2024-11-06 23:04:37 +0530

    layout initialized

--- wms-frontend/src/pages/home/index.vue
+++ wms-frontend/src/pages/home/index.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>Dashboard</h1>
+	<div>
+		<div class="grid grid-cols-2 gap-4">
+			<Card>
+				<template #title>Product Status</template>
+			</Card>
+			<Card>
+				<template #title>Money Flow</template>
+			</Card>
+		</div>
+		<div class="grid grid-cols-3 gap-4 mt-4">
+			<Card>
+				<template #title>Accounts Receivable(Sales)</template>
+			</Card>
+			<Card>
+				<template #title>Accounts Payable(Purchases)</template>
+			</Card>
+			<Card>
+				<template #title>Turn Over</template>
+			</Card>
+		</div>
+	</div>
 </template>
